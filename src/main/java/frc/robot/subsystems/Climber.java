0467c2973d987ca8507931725baf3309dc9a8170
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2024-02-22 14:36:05 -0500

    inverted motors

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -33,8 +33,8 @@ public class Climber extends SubsystemBase {
 //    m_rightClimberMotor.setPosition(0);
 //    m_leftClimberMotor.setPosition(0);
 
-    rightClimberMotor.setInverted(true);
-    leftClimberMotor.setInverted(true);
+    rightClimberMotor.setInverted(false);
+    leftClimberMotor.setInverted(false);
   }
 
   public void periodic() {
