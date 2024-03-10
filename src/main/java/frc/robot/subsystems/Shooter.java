8c904a8a75d0179fb4d16b1031228d2256a3cdc0
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2024-03-09 22:01:24 -0500

    fixed weird shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -43,6 +44,10 @@ public class Shooter extends SubsystemBase {
 //      rightSpin = 1.0;
 //      leftSpin = 0.9;
 //    }
+
+    if (MathUtil.isNear(0, m_leftShooter.getVelocity().getValue(), 0.01)) {
+      setVoltage(0);
+    }
   }
 
   public boolean atSetpoint() {
