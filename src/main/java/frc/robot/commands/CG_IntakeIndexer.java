bd543c5394a63529ea07a3fb0bc5affcd6c03a6f
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2024-03-10 22:42:32 -0400

    removed

--- src/main/java/frc/robot/commands/CG_IntakeIndexer.java
+++ src/main/java/frc/robot/commands/CG_IntakeIndexer.java
@@ -1,15 +1,15 @@
 package frc.robot.commands;
 
 
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.TilterConstants;
 
-public class CG_IntakeIndexer extends ParallelDeadlineGroup {
+public class CG_IntakeIndexer extends ParallelCommandGroup {
     public CG_IntakeIndexer() {
         super(
                 new IndexerRun(),
                 new ShooterSetpointSpeed(0),
-                new TilterSetpointPositionThenPostition(TilterConstants.GROUND_INTAKE_POSITION, TilterConstants.IDLE_POSITION),
+                new TilterSetpointPosition(TilterConstants.GROUND_INTAKE_POSITION),
                 new IntakeRun()
         );
     }
