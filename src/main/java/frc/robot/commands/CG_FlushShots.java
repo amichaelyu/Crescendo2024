5bad557cd570cb1aab303a7e8f523f1dd5b699ec
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2024-03-12 21:08:50 -0400

    added flush fix

--- src/main/java/frc/robot/commands/CG_FlushShots.java
+++ src/main/java/frc/robot/commands/CG_FlushShots.java
@@ -2,10 +2,14 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.TilterConstants;
 
 public class CG_FlushShots extends ParallelCommandGroup {
     public CG_FlushShots() {
         super(
+                new TilterSetpointPosition(TilterConstants.GROUND_INTAKE_POSITION),
+                new ShooterSetpointSpeed(ShooterConstants.INTAKE_SPEED),
                 new IntakeReverse(),
                 new IndexerFlush()
         );
