132e4c4556cda46431c4988083837fa297c88215
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2024-03-12 20:32:39 -0400

    path updates

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,7 @@ public final class Constants {
 
     public static final class IndexerConstants {
         public static final int ID = 22;
-        public static final double FORWARD_DUTY_CYCLE = -0.7;
+        public static final double FORWARD_DUTY_CYCLE = -1.0;
         public static final double FLUSH_DUTY_CYCLE = 0.75;
         
         public static final int CURRENT_CYCLES = 20;
