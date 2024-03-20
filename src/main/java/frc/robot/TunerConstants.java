e3da93e842a5c5923cadcb51dba2b4342791d203
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2024-03-19 22:53:22 -0400

    quick fix

--- src/main/java/frc/robot/TunerConstants.java
+++ src/main/java/frc/robot/TunerConstants.java
@@ -23,8 +23,8 @@ public class TunerConstants {
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-            .withKP(0.52307).withKI(0).withKD(0)
-            .withKS(0.0906094).withKV(2.875).withKA(0.22967);
+            .withKP(0.52307).withKI(0).withKD(0);
+//            .withKS(0.0906094).withKV(2.875).withKA(0.22967);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
